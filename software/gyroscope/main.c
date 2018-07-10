034f3dfe89632945d36f1ad8cb16460c7463222c
Brandon Dixon <brandon.dixon@sevun.com>
2018-07-10 01:23:56 +0000

    setup active and standby modes

--- software/gyroscope/main.c
+++ software/gyroscope/main.c
@@ -64,33 +64,19 @@ int main()
     }
 
     // Put the device into standby before changing register values
-//    AGStandby(AG_SLAVE_ADDR);
-
-    // Choose the range of the accelerometer (±2G,±4G,±8G)
-//    AGAccelRange(AG_SLAVE_ADDR, AFSR_2G);
-
-    // Choose the output data rate (800 Hz, 400 Hz, 200 Hz, 100 Hz,
-    //  50 Hz, 12.5 Hz, 6.25 Hz, 1.56 Hz). Rate is cut in half when
-    //  running in hybrid mode (accelerometer and magnetometer active)
-//    AGOutputDataRate(AG_SLAVE_ADDR, ODR_6_25HZ);
-
-    // Choose if both the acclerometer and magnetometer will both be used
-    //  IF BOTH ARE USED THAN OUTPUT DATA RATE IS SHARED.
-    //  E.G. 100 HZ ODR MEANS ACCELEROMETER WILL SAMPLE AT 50 HZ
-    //    AND MAGNETOMETER WILL SAMPLE AT 50 HZ
-//    AGHybridMode(AG_SLAVE_ADDR, ACCEL_AND_MAG);
+    GyroStandby(GYRO_SLAVE_ADDR);
 
     // Activate the data device
-//    AGActive(AG_SLAVE_ADDR);
+    GyroReady(GYRO_SLAVE_ADDR);
 
-//    AGGetData(AG_SLAVE_ADDR, ACCEL_DATA, &tAccelData );
-//    AGGetData(AG_SLAVE_ADDR, MAG_DATA, &tMagData );
+    // Activate the data device
+    GyroActive(GYRO_SLAVE_ADDR);
 
     // ***********************Print values for testing feedback
-//    printf("\r\nAccelerometer X:%d Y:%d Z:%d",tAccelData.x,tAccelData.y,tAccelData.z);
-//    printf("\r\nMagnetometer  X:%d Y:%d Z:%d",tMagData.x,tMagData.y,tMagData.z);
+    I2CGyroReceive(GYRO_SLAVE_ADDR, GYRO_CTRL_REG1, ui32Data, sizeof(ui32Data));
+    printf("\r\nGYRO_CTRL_REG1=0x%02X",ui32Data[0]);
     // ***********************Print values for testing feedback
-    
+
     printf("\r\n");
     return 0;
 }
